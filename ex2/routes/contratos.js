54ef02857425df9120ec0e02a795b59f69f5e575
a97455 <a97455@alunos.uminho.pt>
2024-05-16 16:44:01 +0100

    entidades a meio

--- ex2/routes/contratos.js
+++ ex2/routes/contratos.js
@@ -13,6 +13,24 @@ router.get('/', function(req, res) {
   })
 });
 
+router.get('/entidades/:id', function(req, res) {
+  var d = new Date().toISOString().substring(0, 16)
+  axios.get(`http://localhost:16000/contratos?entidade=${req.params.id}`)
+    .then(function(response){
+      const contratos = response.data;
+      const entidadeEncontrada = contratos.find(entidade => entidade.NIPC_entidade_comunicante == req.params.id);
+      
+      if (entidadeEncontrada) {
+        res.render('entidade', { title: `Entidade ${entidadeEncontrada.NIPC_entidade_comunicante}`, entidade: entidadeEncontrada, date: d });
+      } else {
+        res.render('error', { error: 'Entidade não encontrada', message: 'Erro ao recuperar os contratos' });
+      }
+    })
+    .catch(function(error){
+      res.render('error', { error: error, message: 'Erro ao recuperar as entidades'});
+    });
+});
+
 router.get('/:id', function(req, res) {
   var d = new Date().toISOString().substring(0, 16)
   axios.get('http://localhost:16000/contratos/'+req.params.id)
